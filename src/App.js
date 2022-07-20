b0febfaa93ada78155a94a3d32e6ffd1abfd7cb0
Chisom Chukwuma <chukwumachisom18@gmail.com>
2022-07-19 21:44:43 -1100

    Another change

--- src/App.js
+++ src/App.js
@@ -425,6 +425,74 @@ const App = () => {
                   )
                 })}
             </section>
+            <section>
+              {data.map((item, key) => {
+                if (clearItem) {
+                  return (
+                    <div key={key} className='border border-danger mt-5'>
+                      <img src={item.logo} alt='logo' />
+                      <p>{item.company}</p>
+                      <p>{item.new === true && <p>new!</p>}</p>
+                      <p>{item.featured === true && <p>featured!</p>}</p>
+                      <p>{item.position}</p>
+                      <p>{item.postedAt}</p>
+                      <p>{item.contract}</p>
+                      <p>{item.location}</p>
+                      <hr />
+                      <div>
+                        <p
+                          onClick={() => (
+                            setKey(false), setNewrole([...newrole, item.role])
+                          )}
+                        >
+                          {item.role}
+                        </p>
+                        <p
+                          onClick={() => (
+                            setKey(false),
+                            setNewlevel([...newlevel, item.level])
+                          )}
+                        >
+                          {item.level}
+                        </p>
+                        <div>
+                          {item.languages.map((item, key) => {
+                            return (
+                              <div key={key}>
+                                <p
+                                  onClick={() => (
+                                    setKey(false),
+                                    setNewlanguage([...newlanguage, item])
+                                  )}
+                                >
+                                  {item}
+                                </p>
+                              </div>
+                            )
+                          })}
+                        </div>
+                        <div>
+                          {item.tools.map((item, key) => {
+                            return (
+                              <div key={key}>
+                                <p
+                                  onClick={() => (
+                                    setKey(false),
+                                    setNewtool([...newtool, item])
+                                  )}
+                                >
+                                  {item}
+                                </p>
+                              </div>
+                            )
+                          })}
+                        </div>
+                      </div>
+                    </div>
+                  )
+                }
+              })}
+            </section>
           </div>
         )}
       </div>
