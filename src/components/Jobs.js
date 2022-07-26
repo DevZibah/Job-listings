6b61752e55d4e82b30731d2e301e775d7cf32fde
Chisom Chukwuma <chukwumachisom18@gmail.com>
2022-07-26 11:31:35 -1100

    css edit

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -13,39 +13,115 @@ const Jobs = ({
   setNewtool,
 }) => {
   return (
-    <section className='border border-dark p-4'>
+    <section className='border border-dark p-3'>
       {data.map((item, key) => {
         return (
-          <div key={key} className='border border-danger mt-5'>
-            <img src={item.logo} alt='logo' />
-            <p>{item.company}</p>
-            <p>{item.new === true && <p>new!</p>}</p>
-            <p>{item.featured === true && <p>featured!</p>}</p>
-            <p>{item.position}</p>
-            <p>{item.postedAt}</p>
-            <p>{item.contract}</p>
-            <p>{item.location}</p>
+          <div
+            key={key}
+            className='mt-5 main-div p-3'
+            style={{
+              borderLeft:
+                item.id === 1
+                  ? '7px solid hsl(180, 29%, 50%)'
+                  : item.id === 2
+                  ? '7px solid hsl(180, 29%, 50%)'
+                  : 'none',
+            }}
+          >
+            <div className='image'>
+              <img src={item.logo} alt='logo' />
+            </div>
+            <div className='grid1 mt-1'>
+              <p
+                className='footPone mt-1'
+                style={{
+                  width:
+                    item.company === 'Loop Studios'
+                      ? '8rem'
+                      : item.company === 'Eyecam Co.'
+                      ? '7rem'
+                      : item.company === 'The Air Filter Company'
+                      ? '13rem'
+                      : 'none',
+                }}
+              >
+                {item.company}
+              </p>
+              <p
+                className=''
+                style={{
+                  border:
+                    item.new === true ? '1px solid hsl(180, 29%, 50%)' : 'none',
+                  backgroundColor:
+                    item.new === true ? 'hsl(180, 29%, 50%)' : 'none',
+                  color: item.new === true ? 'white' : 'none',
+                  fontSize: item.new === true ? '14px' : 'none',
+                  fontWeight: item.new === true ? '600' : 'none',
+                  textTransform: item.new === true ? 'uppercase' : 'none',
+                  width: item.new === true ? '3.6rem' : 'none',
+                  padding: item.new === true ? '.3rem' : 'none',
+                  borderRadius: item.new === true ? '1rem' : 'none',
+                  height: item.new === true ? '2rem' : 'none',
+                  textAlign: item.new === true ? 'center' : 'none',
+                }}
+              >
+                {item.new === true && <p>new!</p>}
+              </p>
+              <p
+                className=''
+                style={{
+                  border:
+                    item.featured === true
+                      ? '1px solid hsl(180, 14%, 20%)'
+                      : 'none',
+                  backgroundColor:
+                    item.featured === true ? 'hsl(180, 14%, 20%)' : 'none',
+                  color: item.featured === true ? 'white' : 'none',
+                  fontSize: item.featured === true ? '14px' : 'none',
+                  fontWeight: item.featured === true ? '600' : 'none',
+                  textTransform: item.featured === true ? 'uppercase' : 'none',
+                  width: item.featured === true ? '6rem' : 'none',
+                  padding: item.featured === true ? '.3rem' : 'none',
+                  borderRadius: item.featured === true ? '1rem' : 'none',
+                  height: item.featured === true ? '2rem' : 'none',
+                  textAlign: item.featured === true ? 'center' : 'none',
+                }}
+              >
+                {item.featured === true && <p>featured</p>}
+              </p>
+            </div>
+            <p className='position'>{item.position}</p>
+            <div className='grid2'>
+              <p>{item.postedAt}</p>
+              <li className='ml-3'>{item.contract}</li>
+              <li className='ml-3'>{item.location}</li>
+            </div>
             <hr />
-            <div>
-              <p
-                onClick={() => (
-                  setKey(false), setNewrole([...newrole, item.role])
-                )}
-              >
-                {item.role}
-              </p>
-              <p
-                onClick={() => (
-                  setKey(false), setNewlevel([...newlevel, item.level])
-                )}
-              >
-                {item.level}
-              </p>
-              <div>
+            <div className='grid4'>
+              <div className='grid3'>
+                <p
+                  className='footP p-1'
+                  onClick={() => (
+                    setKey(false), setNewrole([...newrole, item.role])
+                  )}
+                >
+                  {item.role}
+                </p>
+                <p
+                  className='footP p-1'
+                  onClick={() => (
+                    setKey(false), setNewlevel([...newlevel, item.level])
+                  )}
+                >
+                  {item.level}
+                </p>
+              </div>
+              <div className='grid3'>
                 {item.languages.map((item, key) => {
                   return (
                     <div key={key}>
                       <p
+                        className='footP p-1'
                         onClick={() => (
                           setKey(false), setNewlanguage([...newlanguage, item])
                         )}
@@ -56,11 +132,12 @@ const Jobs = ({
                   )
                 })}
               </div>
-              <div>
+              <div className='grid3'>
                 {item.tools.map((item, key) => {
                   return (
                     <div key={key}>
                       <p
+                        className='footP p-1'
                         onClick={() => (
                           setKey(false), setNewtool([...newtool, item])
                         )}
--- src/components/Searchedjob.js
+++ src/components/Searchedjob.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import close from '../images/icon-remove.svg'
 
 const Searchedjob = ({
   data,
@@ -39,18 +40,20 @@ const Searchedjob = ({
   }
   console.log(uniqueAddArray)
   return (
-    <div>
+    <div className='border border-dark p-3'>
       {' '}
-      <header>
+      <header className='bg-white main-header p-3 mt-n3'>
         {uniqueAddArray.map((item, key) => {
           return (
-            <div key={key}>
-              <p>{item}</p>
-              <p onClick={() => deleteItem(item)}>close</p>
+            <div key={key} className='flexx'>
+              <p className='itemm p-1'>{item}</p>
+              <div className='fleximg'>
+                <img className='mt-1' onClick={() => deleteItem(item)} src={close} alt='close' />
+              </div>
             </div>
           )
         })}
-        <p onClick={clearItem}>clear</p>
+        <p className='cleear mt-1' onClick={clearItem}>Clear</p>
       </header>
       <section>
         {data
@@ -74,36 +77,113 @@ const Searchedjob = ({
           })
           .map((item, key) => {
             return (
-              <div key={key} className='border border-danger mt-5'>
-                <img src={item.logo} alt='logo' />
-                <p>{item.company}</p>
-                <p>{item.new === true && <p>new!</p>}</p>
-                <p>{item.featured === true && <p>featured!</p>}</p>
-                <p>{item.position}</p>
-                <p>{item.postedAt}</p>
-                <p>{item.contract}</p>
-                <p>{item.location}</p>
+              <div
+                key={key}
+                className='mt-5 main-div p-3'
+                style={{
+                  borderLeft:
+                    item.id === 1
+                      ? '7px solid hsl(180, 29%, 50%)'
+                      : item.id === 2
+                      ? '7px solid hsl(180, 29%, 50%)'
+                      : 'none',
+                }}
+              >
+                <div className='image'>
+                  <img src={item.logo} alt='logo' />
+                </div>
+                <div className='grid1 mt-1'>
+                  <p
+                    className='footPone mt-1'
+                    style={{
+                      width:
+                        item.company === 'Loop Studios'
+                          ? '8rem'
+                          : item.company === 'Eyecam Co.'
+                          ? '7rem'
+                          : item.company === 'The Air Filter Company'
+                          ? '13rem'
+                          : 'none',
+                    }}
+                  >
+                    {item.company}
+                  </p>
+                  <p
+                    style={{
+                      border:
+                        item.new === true
+                          ? '1px solid hsl(180, 29%, 50%)'
+                          : 'none',
+                      backgroundColor:
+                        item.new === true ? 'hsl(180, 29%, 50%)' : 'none',
+                      color: item.new === true ? 'white' : 'none',
+                      fontSize: item.new === true ? '14px' : 'none',
+                      fontWeight: item.new === true ? '600' : 'none',
+                      textTransform: item.new === true ? 'uppercase' : 'none',
+                      width: item.new === true ? '3.6rem' : 'none',
+                      padding: item.new === true ? '.3rem' : 'none',
+                      borderRadius: item.new === true ? '1rem' : 'none',
+                      height: item.new === true ? '2rem' : 'none',
+                      textAlign: item.new === true ? 'center' : 'none',
+                    }}
+                  >
+                    {item.new === true && <p>new!</p>}
+                  </p>
+                  <p
+                    style={{
+                      border:
+                        item.featured === true
+                          ? '1px solid hsl(180, 14%, 20%)'
+                          : 'none',
+                      backgroundColor:
+                        item.featured === true ? 'hsl(180, 14%, 20%)' : 'none',
+                      color: item.featured === true ? 'white' : 'none',
+                      fontSize: item.featured === true ? '14px' : 'none',
+                      fontWeight: item.featured === true ? '600' : 'none',
+                      textTransform:
+                        item.featured === true ? 'uppercase' : 'none',
+                      width: item.featured === true ? '6rem' : 'none',
+                      padding: item.featured === true ? '.3rem' : 'none',
+                      borderRadius: item.featured === true ? '1rem' : 'none',
+                      height: item.featured === true ? '2rem' : 'none',
+                      textAlign: item.featured === true ? 'center' : 'none',
+                    }}
+                  >
+                    {item.featured === true && <p>featured!</p>}
+                  </p>
+                </div>
+                <p className='position'>{item.position}</p>
+                <div className='grid2'>
+                  <p>{item.postedAt}</p>
+                  <li className='ml-3'>{item.contract}</li>
+                  <li className='ml-3'>{item.location}</li>
+                </div>
                 <hr />
-                <div>
-                  <p
-                    onClick={() => (
-                      setKey(false), setNewrole([...newrole, item.role])
-                    )}
-                  >
-                    {item.role}
-                  </p>
-                  <p
-                    onClick={() => (
-                      setKey(false), setNewlevel([...newlevel, item.level])
-                    )}
-                  >
-                    {item.level}
-                  </p>
-                  <div>
+                <div className='grid4'>
+                  <div className='grid3'>
+                    <p
+                      className='footP p-1'
+                      onClick={() => (
+                        setKey(false), setNewrole([...newrole, item.role])
+                      )}
+                    >
+                      {item.role}
+                    </p>
+                    <p
+                      className='footP p-1'
+                      onClick={() => (
+                        setKey(false), setNewlevel([...newlevel, item.level])
+                      )}
+                    >
+                      {item.level}
+                    </p>
+                  </div>
+                  <div className='grid3'>
                     {item.languages.map((item, key) => {
                       return (
                         <div key={key}>
                           <p
+                            className='footP p-1'
                             onClick={() => (
                               setKey(false),
                               setNewlanguage([...newlanguage, item])
@@ -115,11 +195,12 @@ const Searchedjob = ({
                       )
                     })}
                   </div>
-                  <div>
+                  <div className='grid3'>
                     {item.tools.map((item, key) => {
                       return (
                         <div key={key}>
                           <p
+                            className='footP p-1'
                             onClick={() => (
                               setKey(false), setNewtool([...newtool, item])
                             )}
